feebc17531ebd8594d5f5bcec9294064d11a5718
Nilesh  Kumar Singh <nileshkumarsingh9245@gmail.com>
2025-04-07 23:50:57 +0530

    updated onBoardingScreen

--- lib/on_borading/presentation/on_boarding_screen.dart
+++ lib/on_borading/presentation/on_boarding_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sharepic/core/utilities/constants.dart';
 
 class OnBoardingScreen extends StatelessWidget {
+  //var column;
+
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: primaryBackgroundColor,
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            width: Get.width,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Color(0xff347AF6).withValues(alpha: 0.2),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Kwikpic'),
+            ],
+          ),
+          Container(
+            width: Get.width * 0.7,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: primaryBackgroundColor.withValues(alpha: 0.8),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
